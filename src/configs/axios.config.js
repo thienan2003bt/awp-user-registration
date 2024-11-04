f3167e96e3c15bbf5cf79fe6f0397f4b880118dc
21120036 <thienan2003bt@gmail.com>
2024-11-04 23:45:17 +0700

    Config axios to automatically invoke new tokens when access token is expired

--- src/configs/axios.config.js
+++ src/configs/axios.config.js
@@ -15,9 +15,28 @@ instance.interceptors.request.use((config) => {
 //RESPONSE INTERCEPTOR
 instance.interceptors.response.use((response) => {
     return response.data;
-}, (err) => {
-    const data = err?.response?.data;
+}, async (error) => {
+    const data = error?.response?.data;
     let msg = data?.message ?? "";
+
+    const originalRequest = error.config;
+
+    if (error.response && error.response.status === 401 && error.response.data.message === 'Token has expired!') {
+      // Token has expired, request a new token pair
+        try {
+            const response = await instance.get('/user/invoke-new-tokens');
+            if (!response || !response.data || !response.data.accessToken) {
+                throw new  Error('Cannot calling API for refreshing tokens');
+            }
+            
+            originalRequest.headers['Authorization'] = `${response.data.accessToken}`;
+
+            return instance(originalRequest);
+        } catch (refreshError) {
+            console.error('Failed to refresh token:', refreshError);
+            window.location.href = '/login';
+        }
+    }
 
     return data ?? Promise.reject(new Error(msg ?? ''));
 });
